9d0f9ded51cc11d1363ef8c31d7c89397967ca0a
Adam Duff <j.adam.duff@gmail.com>
2018-08-17 18:26:29 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,10 +1,15 @@
 class Owner
   attr_reader :species
-  attr_accessor :name
+  attr_accessor :name, :pets
   @@all = []
 
   def initialize(species)
     @species = species
+    @pets = {
+      :fishes => [],
+      :dogs => [],
+      :cats => []
+    }
   end
 
   def all
@@ -21,6 +26,21 @@ class Owner
 
   def say_species
     puts "I am a #{self.species}."
+  end
+
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    self.pets[:fishes] << new_fish
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    self.pets[:dogs] << new_dog
+  end
+
+  def buy_cat(name)
+    new_cat = cat.new(name)
+    self.pets[:cats] << new_cat
   end
 
 end
